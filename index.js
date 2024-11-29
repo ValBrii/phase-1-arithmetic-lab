07d0f4d65e141d807bebf9ba0798c0ded8cc0c38
Valerie <britneyvalerie85@gmail.com>
2024-11-29 10:54:36 +0300

    arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result: 62
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+let num5 = 5
+let num6 = 10
+let num7 = 20
+let max = Math.max(5 ,10, 20)
